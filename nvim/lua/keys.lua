e8fb0889af27e4fe298a60eaef558bcd52b83a86
Sérgio Sarabando <ssarabando@gmail.com>
2023-09-27 20:54:46 +0100

    Remove the keybindings for the terminal.
    Won't be using an integrated terminal for now.

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -2,14 +2,6 @@
 
 local map = vim.api.nvim_set_keymap
 
--- TERMINAL
--- Terminal: new
--- map('n', "<leader>tn", ":FloatermNew --name=Terminal --height=0.8 --width=0.7 --autoclose=2 pwsh <CR> ", {})
--- Terminal: attach
--- map('n', "<leader>ta", ":FloatermToggle Terminal<CR>", {})
--- Terminal: dettach
--- map('t', "<Esc>", "<C-\\><C-n>:q<CR>", {})
-
 -- FUZZY FINDER
 map('n', "<leader>ff", ":Telescope find_files<CR>", {})
 map('n', "<leader>fg", ":Telescope live_grep<CR>", {})

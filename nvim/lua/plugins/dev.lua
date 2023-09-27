fd36159a6b43e66b3a906d0c6f091a452cd74745
Sérgio Sarabando <ssarabando@gmail.com>
2023-09-27 18:44:14 +0100

    Adds LSP configuration, ZLS and Trouble.

--- nvim/lua/plugins/dev.lua
+++ nvim/lua/plugins/dev.lua
@@ -5,4 +5,20 @@ return {
     "lewis6991/gitsigns.nvim",
     -- Git commit history
     "junegunn/gv.vim",
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require'lspconfig'.zls.setup{}
+        end,
+        dependencies = {
+            "ziglang/zig.vim",
+            "nvim-lua/completion-nvim",
+        },
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
 }

971a63967a842e91cee358dcdff3034fcc2383bd
Sérgio Sarabando <ssarabando@gmail.com>
2023-09-27 16:09:42 +0100

    Adds FZF to Telescope.

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,6 +1,10 @@
 return {
     -- Treesitter
     {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         build = function()
             require("nvim-treesitter.install").update({ with_sync = true })()

0bfc72535163782d8dea7c35a9812668d938135f
Sérgio Sarabando <ssarabando@gmail.com>
2023-09-27 21:06:19 +0100

    Surround plugin.

--- nvim/lua/plugins/dev.lua
+++ nvim/lua/plugins/dev.lua
@@ -1,7 +1,11 @@
 return {
     -- Vim plugin for Git
+    -- https://github.com/tpope/vim-fugitive
     "tpope/vim-fugitive",
+    -- https://github.com/tpope/vim-surround
+    "tpope/vim-surround",
     -- Git decorations
+    -- https://github.com/lewis6991/gitsigns.nvim
     "lewis6991/gitsigns.nvim",
     -- Git commit history
     "junegunn/gv.vim",
@@ -13,7 +17,7 @@ return {
         end,
         dependencies = {
             "ziglang/zig.vim",
-            "nvim-lua/completion-nvim",
+            -- "nvim-lua/completion-nvim",
         },
     },
     -- Pretty list for showing diagnostics, etc.

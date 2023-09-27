52c4019ef3e232f34f8899017f4ccae762a08340
Sérgio Sarabando <ssarabando@gmail.com>
2023-09-27 20:27:39 +0100

    Integration of FZF with Telescope.

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,20 +1,31 @@
 return {
     -- Treesitter
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    },
+    -- https://github.com/nvim-treesitter/nvim-treesitter
     {
         "nvim-treesitter/nvim-treesitter",
         build = function()
             require("nvim-treesitter.install").update({ with_sync = true })()
         end,
     },
+    -- Telescope (fuzzy finder over lists)
+    -- https://github.com/nvim-telescope/telescope.nvim
     {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
+        },
+    },
+    -- FZF (fuzzy finder)
+    -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        config = function()
+            require("telescope").load_extension("fzf")
+        end,
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
         },
     },
 }

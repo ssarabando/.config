e02c06ee9f9f5a6a76ae0e377505e049936db9c1
Sérgio Sarabando <ssarabando@gmail.com>
2023-09-28 15:58:40 +0100

    Adds keymaps to move lines up or down.

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -10,6 +10,14 @@ map('n', "<leader>fh", ":Telescope help_tags<CR>", {})
 map('n', "<leader>fd", ":Telescope lsp_definitions<CR>", {})
 map('n', "<leader>fr", ":Telescope lsp_references<CR>", {})
 
+-- Move lines
+-- https://vimtricks.com/p/vimtrick-moving-lines/
+map("n", "<c-j>", ":m .+1<CR>==", {})
+map("n", "<c-k>", ":m .-2<CR>==", {})
+map("i", "<c-j>", "<Esc>:m .+1<CR>==gi", {})
+map("i", "<c-k>", "<Esc>:m .-2<CR>==gi", {})
+map("v", "<c-j>", ":m '>+1<CR>gv=gv", {})
+map("v", "<c-k>", ":m '<-2<CR>gv=gv", {})
 -- CTAGS VIEWER: tagbar
 -- show classes
 -- map('n', "<leader>sc", ":TagbarToggle<CR>", {})

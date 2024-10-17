37d53b9ac3d8051621e41ca2587b7397ccea055c
Sérgio Sarabando <ssarabando@gmail.com>
2024-10-17 08:37:35 +0100

    chore(psprofile): remove helpers for komorebi and add a new one for git add -p

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,20 +13,6 @@ Function freespace {
 }
 Set-Alias -Name df -Value freespace
 
-Function komorebi_start {
-    komorebic start --whkd
-}
-Function komorebi_stop {
-    komorebic stop --whkd
-}
-Function komorebi_reload {
-    komorebic fetch-asc
-    komorebic reload-configuration
-}
-Set-Alias -Name kstart -Value komorebi_start
-Set-Alias -Name kstop -Value komorebi_stop
-Set-Alias -Name kreload -Value komorebi_reload
-
 Function vs22 {
     Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
     Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Professional\"
@@ -51,6 +37,9 @@ Set-Alias -Name gln -Value git_log
 Function git_status { git status }
 Set-Alias -Name gs -Value git_status
 
+Function git_add_prompt { git add -p }
+Set-Alias -Name gap -Value git_add_prompt
+
 # Notepad++
 
 if (Test-Path "$env:ProgramFiles/Notepad++/notepad++.exe") {
@@ -68,16 +57,6 @@ if (Test-Path "$env:LocalAppData/Programs/Azure Data Studio/azuredatastudio.exe"
     }
     Set-Alias -Name ads -Value azure_data_studio
 }
-
-# Komorebi
-
-$Env:KOMOREBI_CONFIG_HOME = "$Env:USERPROFILE\.config\komorebi"
-
-Function komorebi_start { komorebic start --whkd } 
-Set-Alias -Name kstart -Value komorebi_start
-
-Function komorebi_stop { komorebic stop --whkd }
-Set-Alias -Name kstop -Value komorebi_stop
 
 oh-my-posh init pwsh --config "$env:USERPROFILE\.config\ohmyposh\own-basedon-jandedobbeleer.omp.json" | Invoke-Expression
 

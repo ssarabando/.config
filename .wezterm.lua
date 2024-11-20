03ee3a35e95d46f5818fb7ab757e19f08e39bb1f
Sérgio Sarabando <ssarabando@gmail.com>
2024-11-20 11:57:57 +0000

    fix(wezterm): maximize on startup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,17 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+local config = wezterm.config_builder()
+
+wezterm.on("gui-attached", function(domain)
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
 
 local function file_exists(path)
 	local f = io.open(path, "r")

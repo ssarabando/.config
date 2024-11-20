16c22f621724ef1f15db476f89d71f14c8effcb1
Sérgio Sarabando <ssarabando@gmail.com>
2024-11-20 10:02:58 +0000

    feat(wezterm): tmux keybindings.
    See https://github.com/prabirshrestha/dotfiles/blob/main/.config/wezterm/wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,26 +1,99 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+local function file_exists(path)
+	local f = io.open(path, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local launch_menu = {}
 
 -- This is where you actually apply your config choices
 
 -- Color scheme:
 config.color_scheme = "Dracula (Official)"
+-- Default current working directory
+config.default_cwd = wezterm.home_dir .. "/source"
+-- Disable defaul key bindings
+config.disable_default_key_bindings = true
 -- Fonts:
 config.font = wezterm.font_with_fallback({
 	-- "3270 Nerd Font",
 	"IosevkaTerm NF",
 })
--- Default shell:
-config.default_prog = {
-	"pwsh.exe",
-	"-NoExit",
-	-- "-Command",
-	-- '&{Import-Module "C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 0af32636 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64" }',
+-- ?
+config.inactive_pane_hsb = {
+	hue = 1.0,
+	saturation = 1.0,
+	brightness = 1.0,
 }
--- Default current working directory
-config.default_cwd = wezterm.home_dir .. "/source"
+-- tmux keybindings
+config.keys = {
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "\\", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+
+	{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
+	{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
+	{ key = "+", mods = "SHIFT|CTRL", action = "IncreaseFontSize" },
+	{ key = "-", mods = "SHIFT|CTRL", action = "DecreaseFontSize" },
+	{ key = "0", mods = "SHIFT|CTRL", action = "ResetFontSize" },
+}
+-- Set the leader key
+config.leader = { key = "a", mods = "CTRL" }
+-- Do not set environment variables
+config.set_environment_variables = {}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "pwsh.exe" }
+	table.insert(launch_menu, { label = "PowerShell", args = { "pwsh.exe", "-NoLogo" } })
+
+	-- Find installed visual studio version(s) and add their compilation
+	-- environment command prompts to the menu
+	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+		local year = vsvers:gsub("Microsoft Visual Studio/", "")
+		table.insert(launch_menu, {
+			label = "x64 Native Tools VS " .. year,
+			args = {
+				"cmd.exe",
+				"/k",
+				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+			},
+		})
+	end
+end
+
+config.launch_menu = launch_menu
+
 -- and finally, return the configuration to wezterm
 return config
